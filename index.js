52a3c2fc42cd55bbb13bcd00c160a24d784ffdb7
gui1949 <guilhermesantiago12@gmail.com>
2021-05-17 23:40:31 -0300

    Adicionado tigre

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 const bichos_exec = () => {
-    let bichos = [
-        "avestruz",
-        "aguia",
-        "burro",
-        "borboleta",
-        "cachorro",
-        "cabra",
-        "carneiro",
-        "camelo",
-        "cobra",
-        "coelho",
-        "cavalo",
-        "elefante",
-        "galo",
-        "gato",
-        "jacaré",
-        "leão",
-        "macaco",
-        "porco",
-        "pavão",
-        "peru",
-        "touro",
-        "urso",
-        "veado",
-        "vaca",
-      ];
-      
-      let i = 0;
-      
-      while (i < bichos.length) {
-        let xenix = document.createElement("div");
-        xenix.className = "bicho-container";
-        xenix.id = bichos[i];
-      
-        let imagem = document.createElement("img");
-        imagem.src = "/img/" + bichos[i] + ".jpg";
-        imagem.className = "bichos";
-        xenix.appendChild(imagem);
-      
-        document.getElementById("tabela-bicho").appendChild(xenix)
-      
-        console.log(bichos[i]);
-        i++;
-      }
-}
+  let bichos = [
+    "avestruz",
+    "aguia",
+    "burro",
+    "borboleta",
+    "cachorro",
+    "cabra",
+    "carneiro",
+    "camelo",
+    "cobra",
+    "coelho",
+    "cavalo",
+    "elefante",
+    "galo",
+    "gato",
+    "jacare",
+    "leao",
+    "macaco",
+    "porco",
+    "pavão",
+    "peru",
+    "touro",
+    "tigre",
+    "urso",
+    "veado",
+    "vaca",
+  ];
 
-window.onload = () => bichos_exec()
+  let i = 0;
+
+  while (i < bichos.length) {
+    let xenix = document.createElement("div");
+    xenix.className = "bicho-container";
+    xenix.id = bichos[i];
+
+    let imagem = document.createElement("img");
+    imagem.src = "/img/" + bichos[i] + ".jpg";
+    imagem.className = "bichos";
+    xenix.appendChild(imagem);
+
+    document.getElementById("tabela-bicho").appendChild(xenix);
+
+    console.log(bichos[i]);
+    i++;
+  }
+};
+
+window.onload = () => bichos_exec();
