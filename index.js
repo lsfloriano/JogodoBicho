f33281417be8e3b0509541704b9bf6508016a841
gui1949 <guilhermesantiago12@gmail.com>
2021-05-20 19:27:38 -0300

    JS7 inventado

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
-const bichos_exec = () => {
-  let bichos = [
-    "avestruz",
-    "aguia",
-    "burro",
-    "borboleta",
-    "cachorro",
-    "cabra",
-    "carneiro",
-    "camelo",
-    "cobra",
-    "coelho",
-    "cavalo",
-    "elefante",
-    "galo",
-    "gato",
-    "jacaré",
-    "leão",
-    "macaco",
-    "porco",
-    "pavão",
-    "peru",
-    "touro",
-    "tigre",
-    "urso",
-    "veado",
-    "vaca",
-  ];
+let bichos = [
+  "avestruz",
+  "aguia",
+  "burro",
+  "borboleta",
+  "cachorro",
+  "cabra",
+  "carneiro",
+  "camelo",
+  "cobra",
+  "coelho",
+  "cavalo",
+  "elefante",
+  "galo",
+  "gato",
+  "jacaré",
+  "leão",
+  "macaco",
+  "porco",
+  "pavão",
+  "peru",
+  "touro",
+  "tigre",
+  "urso",
+  "veado",
+  "vaca",
+];
 
-  let dezenas = [
-    "01 02 03 04",
-    "05 06 07 08",
-    "09 10 11 12",
-    "13 14 15 16",
-    "17 18 19 20",
-    "21 22 23 24",
-    "25 26 27 28",
-    "29 30 31 32",
-    "33 34 35 36",
-    "37 38 39 40",
-    "41 42 43 44",
-    "45 46 47 48",
-    "49 50 51 52",
-    "53 54 55 56",
-    "57 58 59 60",
-    "61 62 63 64",
-    "65 66 67 68",
-    "69 70 71 72",
-    "73 74 75 76",
-    "77 78 79 80",
-    "81 82 83 84",
-    "85 86 87 88",
-    "89 90 91 92",
-    "93 94 95 96",
-    "97 98 99 00",
-  ];
+let dezenas = [
+  "01 02 03 04",
+  "05 06 07 08",
+  "09 10 11 12",
+  "13 14 15 16",
+  "17 18 19 20",
+  "21 22 23 24",
+  "25 26 27 28",
+  "29 30 31 32",
+  "33 34 35 36",
+  "37 38 39 40",
+  "41 42 43 44",
+  "45 46 47 48",
+  "49 50 51 52",
+  "53 54 55 56",
+  "57 58 59 60",
+  "61 62 63 64",
+  "65 66 67 68",
+  "69 70 71 72",
+  "73 74 75 76",
+  "77 78 79 80",
+  "81 82 83 84",
+  "85 86 87 88",
+  "89 90 91 92",
+  "93 94 95 96",
+  "97 98 99 00",
+];
+
+const bichos_exec = () => {
 
   let i = 0;
 
@@ -104,7 +105,19 @@ const paulinho_gogo = (elem) => {
   modus_operandi.shift();
   modus_operandi.push(elem.id);
   document.getElementById(modus_operandi[0]).style.backgroundColor = "#bdbdbd";
+
+  if(elem.id === 'grupo'){
+    let klevitz = 0;
+    while (klevitz < 25) {
+      document.getElementById(bichos[klevitz]).onclick = function () { xablix(this.id); };
+      klevitz++; 
+    }
+  }
 };
+
+const xablix = (elem) => {
+  console.log(elem)
+}
 
 const jubileu = () => {
   if (document.getElementById("xerupita")) {
@@ -130,3 +143,4 @@ const insertNumeros = (elem) => {
 };
 
 window.onload = () => bichos_exec();
+
